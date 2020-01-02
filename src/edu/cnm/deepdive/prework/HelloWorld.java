c6b85de99158d29a67f015aab0ef9034df5c8bf3
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-01-01 20:32:19 -0700

    Added salute method and edited the command configuration

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -2,9 +2,9 @@ package edu.cnm.deepdive.prework;
 
 public class HelloWorld {
   public static void main(String[] args){
-    salute("World!");
-  }
-  private static void salute(String target){
+      salute((args.length > 0) ? args[0] : "World");
+    }
+  public static void salute(String target){
     System.out.println("Hello, "+ target + "!");
   }
 }
